01894608a60459d46ebb4c65ae125ec2690e21f5
polina1201 <polina.melvin1201@gmail.com>
2022-09-27 19:51:56 -0400

    second commit

--- src/class03B14/class03.java
+++ src/class03B14/class03.java
@@ -5,6 +5,7 @@ public class class03 {
 	public static void main(String[] args) {
 		
 System.out.println("Hey this is a git class");	
+System.out.println("Hey this is a change in my project");
 	
 
 
